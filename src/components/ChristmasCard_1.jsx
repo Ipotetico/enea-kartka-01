37c509a2e028ac989f5bb055a2cdee8c4e862ebb
Marcin Zbrożek <m.zbrozek@kiciu.ad.agencjakore.pl>
2020-11-08 19:13:10 +0100

    Zmiany 08-11-20___01

--- src/components/ChristmasCard_1.jsx
+++ src/components/ChristmasCard_1.jsx
@@ -11,17 +11,21 @@ const ChristmasCard_1 = ({ card }) => {
     <>
       <div className="container">
         <MovieScreen title="ENEA_swieta_1" />
-        <div onClick={(handleSetSwicz, card)} className="switch on">
-          <img
-            className={switcz ? "switch" : "switch on"}
-            src={process.env.PUBLIC_URL + `/images/off.svg`}
-            alt="off"
-          />
-          <img
-            className={switcz ? "switch on" : "switch"}
-            src={process.env.PUBLIC_URL + `/images/on.svg`}
-            alt="on"
-          />
+        <div className="box">
+          <div className=""></div>
+          <div onClick={(handleSetSwicz, card)} className="switch on">
+            <img
+              className={switcz ? "switch" : "switch on"}
+              src={process.env.PUBLIC_URL + `/images/off.svg`}
+              alt="off"
+            />
+            <img
+              className={switcz ? "switch on" : "switch"}
+              src={process.env.PUBLIC_URL + `/images/on.svg`}
+              alt="on"
+            />
+          </div>
+          <div className=""></div>
         </div>
       </div>
     </>
